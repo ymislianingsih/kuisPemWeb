94e81eb065ae2011a62be881dc260db87609be89
Yuliana Mislianingsih <113813872+ymislianingsih@users.noreply.github.com>
2024-03-26 15:40:34 +0700

    ini commit
    kuis 1

---
+++ kuis1/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const colorButtons = document.querySelectorAll('.color-button');
+    
+    colorButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const randomColor = getRandomColor();
+            document.body.style.backgroundColor = randomColor;
+        });
+    });
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
